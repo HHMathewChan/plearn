f0a918cc9cb04455bdf264dd379755f0cc96df36
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2025-05-30 11:35:47 +0100

    feat: 02_create_domains.sql ver 1.1 for first complete

--- sql/init/02_create_domains.sql
+++ sql/init/02_create_domains.sql
@@ -1,54 +1,67 @@
--- DOMAIN: CourseCode
-CREATE DOMAIN CourseCode AS VARCHAR(20)
-CHECK (VALUE ~ '^CRS-[A-Z]{2,5}[0-9]{3}$');
+-- DOMAIN: ContentTitle
+CREATE DOMAIN ContentTitle AS VARCHAR(150);
 
--- DOMAIN: EnrolmentCode
-CREATE DOMAIN EnrolmentCode AS VARCHAR(20)
-CHECK (VALUE ~ '^ENR-[0-9]{4}-[0-9]{4}$');
-
--- DOMAIN: PersonNames
-CREATE DOMAIN PersonNames AS VARCHAR(100)
+-- DOMAIN: CopyrightOwnerNames
+CREATE DOMAIN CopyrightOwnerNames AS VARCHAR(255)
 CHECK (VALUE ~ '^[A-Za-z ]+$');
-
--- DOMAIN: EmailAddress
-CREATE DOMAIN EmailAddress AS VARCHAR(255)
-CHECK (VALUE ~ '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$');
-
--- DOMAIN: PasswordHash
-CREATE DOMAIN PasswordHash AS TEXT;
-
--- DOMAIN: LicenseType
-CREATE DOMAIN LicenseType AS VARCHAR(20)
-CHECK (VALUE IN ('OER', 'CC', 'proprietary'));
 
 -- DOMAIN: CopyrightOwnerType
 CREATE DOMAIN CopyrightOwnerType AS VARCHAR(20)
 CHECK (VALUE IN ('individual', 'institution', 'third_party'));
 
--- DOMAIN: ModeName
-CREATE DOMAIN ModeName AS VARCHAR(20)
-CHECK (VALUE IN ('Beginner', 'Intermediate', 'Challenging'));
-
--- DOMAIN: TopicName
-CREATE DOMAIN TopicName AS TEXT;
+-- DOMAIN: CourseCode
+CREATE DOMAIN CourseCode AS VARCHAR(20)
+CHECK (VALUE ~ '^CRS-[A-Z]{2,5}[0-9]{3}$');
 
 -- DOMAIN: CourseTitle
 CREATE DOMAIN CourseTitle AS VARCHAR(150);
 
--- DOMAIN: ContentTitle
-CREATE DOMAIN ContentTitle AS VARCHAR(150);
+-- DOMAIN: EmailAddress
+CREATE DOMAIN EmailAddress AS VARCHAR(255)
+CHECK (VALUE ~ '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$');
+
+-- DOMAIN: EnrolmentCode
+CREATE DOMAIN EnrolmentCode AS VARCHAR(20)
+CHECK (VALUE ~ '^ENR-[0-9]{4}-[0-9]+$');
+
+-- DOMAIN: InterestLevel
+CREATE DOMAIN InterestLevel AS VARCHAR(20)
+CHECK (VALUE IN ('low', 'medium', 'high'));
+
+-- DOMAIN: KnowledgeProficiency
+CREATE DOMAIN KnowledgeProficiency AS VARCHAR(20)
+CHECK (VALUE IN ('novice', 'intermediate', 'proficient'));
+
+-- DOMAIN: LicenseType
+CREATE DOMAIN LicenseType AS VARCHAR(20)
+CHECK (VALUE IN ('OER', 'CC', 'proprietary'));
+
+-- DOMAIN: ModeName
+CREATE DOMAIN ModeName AS VARCHAR(20)
+CHECK (VALUE IN ('beginner', 'intermediate', 'challenging'));
+
+-- DOMAIN: PasswordHash
+CREATE DOMAIN PasswordHash AS TEXT;
+
+-- DOMAIN: PersonNames
+CREATE DOMAIN PersonNames AS VARCHAR(100)
+CHECK (VALUE ~ '^[A-Za-z ]+$');
 
 -- DOMAIN: ProgressStatus
 CREATE DOMAIN ProgressStatus AS VARCHAR(20)
 CHECK (VALUE IN ('not_started', 'in_progress', 'completed'));
 
--- DOMAIN: UserRole
-CREATE DOMAIN UserRole AS VARCHAR(20)
-CHECK (VALUE IN ('student', 'tutor', 'admin'));
+-- DOMAIN: StudentCode
+CREATE DOMAIN StudentCode AS VARCHAR(20)
+CHECK (VALUE ~ '^STR[0-9]{4}[0-9]+$');
+
+-- DOMAIN: TopicName
+CREATE DOMAIN TopicName AS TEXT;
 
 -- DOMAIN: URL
 CREATE DOMAIN URL AS TEXT
 CHECK (VALUE ~ '^https?:\/\/');
 
--- DOMAIN: TimestampNotNull
-CREATE DOMAIN TimestampNotNull AS TIMESTAMP NOT NULL;
+-- DOMAIN: UserRole
+CREATE DOMAIN UserRole AS VARCHAR(20)
+CHECK (VALUE IN ('student', 'tutor', 'admin'));
