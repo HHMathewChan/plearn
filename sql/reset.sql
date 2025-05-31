aedffd0bbea63a622b5ed049529ba14f2b175124
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2025-05-31 11:41:18 +0100

    feat: standardize file naming in reset script

--- sql/reset.sql
+++ sql/reset.sql
@@ -1,9 +1,9 @@
 -- Step one: drop first
-\i drop/01_drop_tables.sql
-\i drop/02a_drop_sequences.sql
-\i drop/02_drop_domains.sql
+\i drop/drop_tables.sql
+\i drop/drop_sequences.sql
+\i drop/drop_domains.sql
 
 -- Step two: recreate
-\i init/02_create_domains.sql
-\i init/02a_create_sequences.sql
-\i init/03_create_tables.sql
+\i init/create_domains.sql
+\i init/create_sequences.sql
+\i init/create_tables.sql
