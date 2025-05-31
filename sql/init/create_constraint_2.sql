a24c4ef1a5ac6cfab22d1337ebe2496a446794e4
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2025-05-31 21:23:44 +0100

    feat: update create_constraint_2.sql to renamed tables

--- sql/init/create_constraint_2.sql
+++ sql/init/create_constraint_2.sql
@@ -1,56 +1,69 @@
--- Constraints for create_table_2.sql
-ALTER TABLE HasStudentProfileIn
-    ADD CONSTRAINT fk_hspi_student FOREIGN KEY (student_code) REFERENCES Student(student_code),
-    ADD CONSTRAINT fk_hspi_user FOREIGN KEY (user_id) REFERENCES "User"(id);
-
-ALTER TABLE KeepCourseProgressWith
-    ADD CONSTRAINT fk_kcpw_course_progress FOREIGN KEY (course_progress_id) REFERENCES CourseProgress(id),
-    ADD CONSTRAINT fk_kcpw_student FOREIGN KEY (student_code) REFERENCES Student(student_code);
-
-ALTER TABLE KeepContentProgressWith
-    ADD CONSTRAINT fk_kcpw_content_progress FOREIGN KEY (content_progress_id) REFERENCES ContentProgress(id),
-    ADD CONSTRAINT fk_kcpw_student FOREIGN KEY (student_code) REFERENCES Student(student_code);
-
-ALTER TABLE LogCourseProgressAt
-    ADD CONSTRAINT fk_lcpa_course FOREIGN KEY (course_id) REFERENCES Course(id),
-    ADD CONSTRAINT fk_lcpa_course_progress FOREIGN KEY (course_progress_id) REFERENCES CourseProgress(id);
-
-ALTER TABLE ChooseLearningModeWith
-    ADD CONSTRAINT fk_clmw_learning_mode FOREIGN KEY (learning_mode_id) REFERENCES LearningMode(id),
-    ADD CONSTRAINT fk_clmw_student FOREIGN KEY (student_code) REFERENCES Student(student_code);
-
-ALTER TABLE InterestedIn
-    ADD CONSTRAINT fk_ii_chosen_topic FOREIGN KEY (chosen_topic_id) REFERENCES ChosenTopic(id),
-    ADD CONSTRAINT fk_ii_student FOREIGN KEY (student_code) REFERENCES Student(student_code);
-
-ALTER TABLE LogContentProgressAt
-    ADD CONSTRAINT fk_lcpa_course_content FOREIGN KEY (course_content_id) REFERENCES CourseContent(id),
-    ADD CONSTRAINT fk_lcpa_content_progress FOREIGN KEY (content_progress_id) REFERENCES ContentProgress(id);
-
-ALTER TABLE HasTopicReferenceTo
-    ADD CONSTRAINT fk_htrt_chosen_topic FOREIGN KEY (chosen_topic_id) REFERENCES ChosenTopic(id),
-    ADD CONSTRAINT fk_htrt_topic FOREIGN KEY (topic_id) REFERENCES Topic(id);
-
-ALTER TABLE RelateContentAround
-    ADD CONSTRAINT fk_rca_topic FOREIGN KEY (topic_id) REFERENCES Topic(id),
-    ADD CONSTRAINT fk_rca_course_content FOREIGN KEY (course_content_id) REFERENCES CourseContent(id);
-
-ALTER TABLE EnrolsIn
-    ADD CONSTRAINT fk_ei_student FOREIGN KEY (student_code) REFERENCES Student(student_code),
-    ADD CONSTRAINT fk_ei_enrolment FOREIGN KEY (enrolment_id) REFERENCES Enrolment(id);
-
-ALTER TABLE RelateCourseAround
-    ADD CONSTRAINT fk_rca_topic FOREIGN KEY (topic_id) REFERENCES Topic(id),
-    ADD CONSTRAINT fk_rca_enrolment FOREIGN KEY (enrolment_id) REFERENCES Enrolment(id);
-
-ALTER TABLE HasCourseReferenceTo
-    ADD CONSTRAINT fk_hcrt_course FOREIGN KEY (course_id) REFERENCES Course(id),
-    ADD CONSTRAINT fk_hcrt_course_progress FOREIGN KEY (course_progress_id) REFERENCES CourseProgress(id);
-
+-- Comprises
 ALTER TABLE Comprises
-    ADD CONSTRAINT fk_c_course FOREIGN KEY (course_id) REFERENCES Course(id),
-    ADD CONSTRAINT fk_c_course_content FOREIGN KEY (course_content_id) REFERENCES CourseContent(id);
+    ADD FOREIGN KEY (course_id) REFERENCES Course(id),
+    ADD FOREIGN KEY (course_content_id) REFERENCES CourseContent(id);
 
-ALTER TABLE HasCopyrightFor
-    ADD CONSTRAINT fk_hcf_course FOREIGN KEY (course_id) REFERENCES Course(id),
-    ADD CONSTRAINT fk_hcf_copyright_owner FOREIGN KEY (copyright_owner_id) REFERENCES CopyrightOwner(id);
+-- EnrolsIn
+ALTER TABLE EnrolsIn
+    ADD FOREIGN KEY (student_code) REFERENCES Student(student_code),
+    ADD FOREIGN KEY (enrolment_id) REFERENCES Enrolment(id);
+
+-- HasCourseReferenceTo
+ALTER TABLE HasCourseReferenceTo
+    ADD FOREIGN KEY (course_id) REFERENCES Course(id),
+    ADD FOREIGN KEY (course_progress_id) REFERENCES CourseProgress(id);
+
+-- HasLearningModeFor
+ALTER TABLE HasLearningModeFor
+    ADD FOREIGN KEY (learning_mode_id) REFERENCES LearningMode(id),
+    ADD FOREIGN KEY (student_code) REFERENCES Student(student_code);
+
+-- HasStudentProfileIn
+ALTER TABLE HasStudentProfileIn
+    ADD FOREIGN KEY (student_code) REFERENCES Student(student_code),
+    ADD FOREIGN KEY (user_id) REFERENCES User(id);
+
+-- HasTopicReferenceTo
+ALTER TABLE HasTopicReferenceTo
+    ADD FOREIGN KEY (chosen_topic_id) REFERENCES ChosenTopic(id),
+    ADD FOREIGN KEY (topic_id) REFERENCES Topic(id);
+
+-- InterestedIn
+ALTER TABLE InterestedIn
+    ADD FOREIGN KEY (chosen_topic_id) REFERENCES ChosenTopic(id),
+    ADD FOREIGN KEY (student_code) REFERENCES Student(student_code);
+
+-- HasContentProgressFor
+ALTER TABLE HasContentProgressFor
+    ADD FOREIGN KEY (content_progress_id) REFERENCES ContentProgress(id),
+    ADD FOREIGN KEY (student_code) REFERENCES Student(student_code);
+
+-- HasCourseProgressFor
+ALTER TABLE HasCourseProgressFor
+    ADD FOREIGN KEY (course_progress_id) REFERENCES CourseProgress(id),
+    ADD FOREIGN KEY (student_code) REFERENCES Student(student_code);
+
+-- LogContentProgressAt
+ALTER TABLE LogContentProgressAt
+    ADD FOREIGN KEY (course_content_id) REFERENCES CourseContent(id),
+    ADD FOREIGN KEY (content_progress_id) REFERENCES ContentProgress(id);
+
+-- LogCourseProgressAt
+ALTER TABLE LogCourseProgressAt
+    ADD FOREIGN KEY (course_id) REFERENCES Course(id),
+    ADD FOREIGN KEY (course_progress_id) REFERENCES CourseProgress(id);
+
+-- OwnCourseBy
+ALTER TABLE OwnCourseBy
+    ADD FOREIGN KEY (course_id) REFERENCES Course(id),
+    ADD FOREIGN KEY (copyright_owner_id) REFERENCES CopyrightOwner(id);
+
+-- LabelContentWith
+ALTER TABLE LabelContentWith
+    ADD FOREIGN KEY (topic_id) REFERENCES Topic(id),
+    ADD FOREIGN KEY (course_content_id) REFERENCES CourseContent(id);
+
+-- LabelCourseWith
+ALTER TABLE LabelCourseWith
+    ADD FOREIGN KEY (topic_id) REFERENCES Topic(id),
+    ADD FOREIGN KEY (enrolment_id) REFERENCES Enrolment(id);
