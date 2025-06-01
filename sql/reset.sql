0ace8ab114206d2d35879c034462bc43e3c75096
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2025-06-01 09:09:31 +0100

    fix: update file paths in reset.sql for drop and recreate scripts

--- sql/reset.sql
+++ sql/reset.sql
@@ -1,13 +1,14 @@
 -- Step one: drop first
-\i drop/drop_triggers.sql
-\i drop/drop_functions.sql
-\i drop/drop_sequences.sql
-\i drop/drop_tables.sql
-\i drop/drop_domains.sql
+\i /Users/mathewchan/plearn/sql/drop/drop_triggers.sql
+\i /Users/mathewchan/plearn/sql/drop/drop_functions.sql
+\i /Users/mathewchan/plearn/sql/drop/drop_sequences.sql
+\i /Users/mathewchan/plearn/sql/drop/drop_tables.sql
+\i /Users/mathewchan/plearn/sql/drop/drop_domains.sql
 
 -- Step two: recreate
-\i init/create_domains.sql
-\i init/create_tables.sql
-\i init/create_sequences.sql
-\i drop/drop_functions.sql
-\i drop/drop_triggers.sql
+\i /Users/mathewchan/plearn/sql/init/create_domains.sql
+\i /Users/mathewchan/plearn/sql/init/create_tables_1.sql
+\i /Users/mathewchan/plearn/sql/init/create_tables_2.sql
+\i /Users/mathewchan/plearn/sql/init/create_sequences.sql
+\i /Users/mathewchan/plearn/sql/init/create_functions.sql
+\i /Users/mathewchan/plearn/sql/init/triggers/all_xxxcode_trg.sql
