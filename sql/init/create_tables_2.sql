e490ef2cab77c2a13c47e346c0b8261f4c0d8e1f
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2025-06-01 08:11:03 +0100

    feat: add NOT NULL constraints and primary keys to table definitions in create_tables_2.sql

--- sql/init/create_tables_2.sql
+++ sql/init/create_tables_2.sql
@@ -1,85 +1,85 @@
 -- Comprises (Course <-> CourseContent)
 CREATE TABLE Comprises (
-    course_id UUID,
-    course_content_id UUID
+    course_id UUID NOT NULL, 
+    course_content_id UUID PRIMARY KEY
 );
 
 -- EnrolsIn (Student <-> Enrolment)
 CREATE TABLE EnrolsIn (
-    student_code StudentCode,
-    enrolment_id UUID
+    student_code StudentCode NOT NULL,
+    enrolment_id UUID PRIMARY KEY
 );
 
 -- HasCourseReferenceTo (Course <-> CourseProgress)
 CREATE TABLE HasCourseReferenceTo (
-    course_id UUID,
-    course_progress_id UUID
+    course_id UUID NOT NULL,
+    course_progress_id UUID PRIMARY KEY
 );
 
 -- HasLearningModeFor (Student <-> LearningMode)
 CREATE TABLE HasLearningModeFor (
-    learning_mode_id UUID,
-    student_code StudentCode
+    learning_mode_id UUID NOT NULL,
+    student_code StudentCode PRIMARY KEY
 );
 
 -- HasStudentProfileIn (User <-> Student)
 CREATE TABLE HasStudentProfileIn (
-    student_code StudentCode,
-    user_id UUID
+    student_code StudentCode UNIQUE NOT NULL,
+    user_id UUID PRIMARY KEY
 );
 
 -- HasTopicReferenceTo (Topic <-> ChosenTopic)
 CREATE TABLE HasTopicReferenceTo (
-    chosen_topic_id UUID,
-    topic_id UUID
+    chosen_topic_id UUID PRIMARY KEY,
+    topic_id UUID NOT NULL
 );
 
 -- InterestedIn (ChosenTopic <-> Student)
 CREATE TABLE InterestedIn (
-    chosen_topic_id UUID,
-    student_code StudentCode
+    chosen_topic_id UUID PRIMARY KEY,
+    student_code StudentCode NOT NULL
 );
 
 -- HasContentProgressFor (ContentProgress <-> Student)
 CREATE TABLE HasContentProgressFor (
-    content_progress_id UUID,
-    student_code StudentCode
+    content_progress_id UUID PRIMARY KEY,
+    student_code StudentCode NOT NULL
 );
 
 -- HasCourseProgressFor (CourseProgress <-> Student)
 CREATE TABLE HasCourseProgressFor (
-    course_progress_id UUID,
-    student_code StudentCode
+    course_progress_id UUID PRIMARY KEY,
+    student_code StudentCode NOT NULL
 );
 
 -- LogContentProgressAt (CourseContent <-> ContentProgress)
 CREATE TABLE LogContentProgressAt (
-    course_content_id UUID,
-    content_progress_id UUID
+    course_content_id UUID NOT NULL,
+    content_progress_id UUID PRIMARY KEY
 );
 
 -- LogCourseProgressAt (Course <-> CourseProgress)
 CREATE TABLE LogCourseProgressAt (
-    course_id UUID,
-    course_progress_id UUID
+    course_id UUID NOT NULL,
+    course_progress_id UUID PRIMARY KEY
 );
 
 -- OwnCourseBy (Course <-> CopyrightOwner)
 CREATE TABLE OwnCourseBy (
-    course_id UUID,
-    copyright_owner_id UUID
+    course_id UUID NOT NULL,
+    copyright_owner_id UUID PRIMARY KEY
 );
 
 -- LabelContentWith (Topic <-> CourseContent)
 CREATE TABLE LabelContentWith (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    topic_id UUID,
-    course_content_id UUID
+    topic_id UUID NOT NULL,
+    course_content_id UUID NOT NULL
 );
 
 -- LabelCourseWith (Topic <-> Enrolment)
 CREATE TABLE LabelCourseWith (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    topic_id UUID,
-    enrolment_id UUID
+    topic_id UUID NOT NULL,
+    enrolment_id UUID NOT NULL
 );
