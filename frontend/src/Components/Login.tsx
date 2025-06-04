ac584bcb309509a6420c2cfa7ef5affc4d861752
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2025-06-04 18:01:32 +0100

    feat: implement Login component with user authentication functionality

---
+++ frontend/src/Components/Login.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+async function loginUser(credentials) {
+  return fetch('http://localhost:3001/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(credentials)
+  })
+    .then(data => data.json())
+}
+
+const Login: React.FC<LoginProps> = ({ setToken }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const token = await loginUser({
+      username,
+      password
+    });
+    setToken(token);
+  }
+
+  return(
+    <div className="flex flex-col items-center">
+      <h1>Please Log In</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p>Username</p>
+          <input type="text" onChange={e => setUsername(e.target.value)}/>
+        </label>
+        <label>
+          <p>Password</p>
+          <input type="password" onChange={e => setPassword(e.target.value)}/>
+        </label>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Login;
