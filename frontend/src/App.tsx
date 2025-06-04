0ea80466c15da2842600251cb76bacefcba0f5ec
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2025-06-04 18:01:12 +0100

    feat: implement authentication flow with token management in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { useState } from 'react';
+import { BrowserRouter,Routes, Route} from 'react-router-dom';
+
+import Dashboard from './Components/Dashboard';
+import Preference from './Components/Preference';
+import Login from './Components/Login'; 
 
 function App() {
+  const [token, setToken] = useState<string | null>(null);
+  if (!token) {
+    return <Login setToken={setToken} />
+  }
   return (
-    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
-      <h1>This is working</h1>
+    <div  className="p-5">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/preference" element={<Preference />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
