5a83ccb623a70df991ade91c0eabbfdc66586223
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2025-06-06 17:17:47 +0100

    refactor: simplify routing by removing unused components and updating routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,17 @@
-import { useState } from 'react';
 import { BrowserRouter,Routes, Route} from 'react-router-dom';
+import StudentHome from './Pages/StudentHome';
+import LandingPage from './Pages/LandingPage';
+import Login from './UseCases/Login';
 
-import Dashboard from './Components/Dashboard';
-import Preference from './Components/Preference';
-import Login from './Components/Login'; 
 
 function App() {
-  const [token, setToken] = useState<string | null>(null);
-  if (!token) {
-    return <Login setToken={setToken} />
-  }
   return (
     <div  className="p-5">
-      <h1>Application</h1>
       <BrowserRouter>
         <Routes>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/preference" element={<Preference />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/student-home" element={<StudentHome />} />
         </Routes>
       </BrowserRouter>
     </div>
