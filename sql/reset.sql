30a756c4cc729e85e03e544d5c053dfd73b2c9b6
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2025-06-08 21:27:31 +0100

    Revert "fix: update file paths in reset.sql to reflect the new database directory structure"
    This reverts commit 0610d365d97415388880513d5be6df0b14da255d.

--- sql/reset.sql
+++ sql/reset.sql
@@ -1,14 +1,14 @@
 -- Step one: drop first
-\i /Users/mathewchan/plearn/database/drop/drop_triggers.sql
-\i /Users/mathewchan/plearn/database/drop/drop_functions.sql
-\i /Users/mathewchan/plearn/database/drop/drop_sequences.sql
-\i /Users/mathewchan/plearn/database/drop/drop_tables.sql
-\i /Users/mathewchan/plearn/database/drop/drop_domains.sql
+\i /Users/mathewchan/plearn/sql/drop/drop_triggers.sql
+\i /Users/mathewchan/plearn/sql/drop/drop_functions.sql
+\i /Users/mathewchan/plearn/sql/drop/drop_sequences.sql
+\i /Users/mathewchan/plearn/sql/drop/drop_tables.sql
+\i /Users/mathewchan/plearn/sql/drop/drop_domains.sql
 
 -- Step two: recreate
-\i /Users/mathewchan/plearn/database/init/create_domains.sql
-\i /Users/mathewchan/plearn/database/init/create_tables_1.sql
-\i /Users/mathewchan/plearn/database/init/create_tables_2.sql
-\i /Users/mathewchan/plearn/database/init/create_sequences.sql
-\i /Users/mathewchan/plearn/database/init/create_functions.sql
-\i /Users/mathewchan/plearn/database/init/triggers/all_xxxcode_trg.sql
+\i /Users/mathewchan/plearn/sql/init/create_domains.sql
+\i /Users/mathewchan/plearn/sql/init/create_tables_1.sql
+\i /Users/mathewchan/plearn/sql/init/create_tables_2.sql
+\i /Users/mathewchan/plearn/sql/init/create_sequences.sql
+\i /Users/mathewchan/plearn/sql/init/create_functions.sql
+\i /Users/mathewchan/plearn/sql/init/triggers/all_xxxcode_trg.sql
