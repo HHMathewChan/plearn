4951e2752de7ce7ef7a0f4dd97be03857d2ad3c9
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2025-06-08 22:20:28 +0100

    refactor: streamline server.js by removing unused router and database code

--- backend/server.js
+++ backend/server.js
@@ -1,54 +1,19 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors'); 
-const database = require('./database');
+const cors = require('cors');
+const studentRoutes = require('./routes/studentRoutes');
 
 const app = express();
-const router = express.Router();
+const port = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(router);
 
-const port = process.env.PORT || 3001;
-
-/** Endpoint to get all students
- * This endpoint retrieves all students from the database
- * and returns them as a JSON response.
- * If an error occurs, it returns a 500 status with the error message.
- */
-router.get('/api/students', async (request, response, next) => {
-  try {
-    const students = await database.any('SELECT * FROM student');
-    response.json(students);
-  } catch (error) {
-    response.status(500).json({ error: error.message });
-  }
-});
-
-// Endpoint to validate loin credentials, assuming a simple email and password check
-router.post('/api/validate', async (request, response) => {
-  const { email, password } = request.body;
-  console.log(`Login attempt with email: ${email}, password: ${password}`);
-  try {
-    // Here you would typically check the credentials against a database
-    if (email === 'admin@gmail.com' && password === 'password') {
-      console.log('Login successful');
-      response.json({ token: 'test123' });
-    } else {
-      console.log('Invalid credentials');
-      response.status(401).json({ error: 'Invalid credentials' });
-    }
-  } catch (error) {
-    console.error('Error during login:', error);
-    response.status(500).json({ error: 'Internal server error' });
-  }
-});
-
+// Routes
+app.use('/api', studentRoutes);
 
 // Start server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
