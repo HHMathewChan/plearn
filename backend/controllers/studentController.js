e20a1b686fb8e6d15a2ccc8c0fff60742d6d44e4
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2025-06-08 22:21:08 +0100

    feat: add studentController with getAllStudents function

---
+++ backend/controllers/studentController.js
@@ -0,0 +1,14 @@
+const studentService = require('../services/studentService');
+
+const getAllStudents = async (request, response) => {
+  try {
+    const students = await studentService.getAllStudents();
+    response.json(students);
+  } catch (error) {
+    response.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  getAllStudents,
+};
