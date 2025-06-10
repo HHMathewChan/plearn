bfb718958b12649037656cdfcbeb909555e5595a
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2025-06-10 08:46:58 +0100

    fix: add createStudent function to studentRepository for student creation

--- backend/repositories/studentRepository.js
+++ backend/repositories/studentRepository.js
@@ -4,6 +4,13 @@ const queryAllStudents = async () => {
   return database.any('SELECT * FROM student');
 };
 
+// create a new student and return the student_code
+const createStudent = async () => {
+  return database.any(
+    'INSERT INTO student DEFAULT VALUES RETURNING student_code');
+};
+
 module.exports = {
   queryAllStudents,
+  createStudent
 };
