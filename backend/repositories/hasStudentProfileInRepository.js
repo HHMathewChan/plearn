a691ff6f45f2705ac4481a631d956939554ccf62
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2025-06-10 18:33:09 +0100

    fix: comment out debug logs in createHasStudentProfileIn function for cleaner code

--- backend/repositories/hasStudentProfileInRepository.js
+++ backend/repositories/hasStudentProfileInRepository.js
@@ -2,9 +2,9 @@ const database = require('../database');
 
 const createHasStudentProfileIn = async (student_code, platform_user_id) => {
     // for debugging purpose
-    console.log("createHasStudentProfileIn called");
-    console.log("student_code:", student_code);
-    console.log("platform_user_id:", platform_user_id);
+    // console.log("createHasStudentProfileIn called");
+    // console.log("student_code:", student_code);
+    // console.log("platform_user_id:", platform_user_id);
     return database.none(
         `INSERT INTO hasstudentprofilein 
             (student_code, platform_user_id)
