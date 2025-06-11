6c2124200bb531de941f857da286cbe93d36b968
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2025-06-11 20:51:57 +0100

    refactor: remove unused type definitions from vite-env.d.ts

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,10 +1,1 @@
 /// <reference types="vite/client" />
-type LoginProps = {
-  setToken: (token: string) => void;
-};
-
-// credentials type for login
-type Credentials = {
-  email: string;
-  password: string;
-};
