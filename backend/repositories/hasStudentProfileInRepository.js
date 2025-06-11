004edbea0d78eee08bdd4c78b204f333ee97c0dd
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2025-06-11 15:04:03 +0100

    feat: add getStudentCodeByPlatformUserId function to retrieve student code by platform user ID

--- backend/repositories/hasStudentProfileInRepository.js
+++ backend/repositories/hasStudentProfileInRepository.js
@@ -14,7 +14,25 @@ const createHasStudentProfileIn = async (student_code, platform_user_id) => {
     );
 }
 
+/**
+ * retrieve the student_code by platform_user_id
+ * @param {string} platform_user_id - The ID of the platform user.
+ * @returns {Promise<string>} - The student_code associated with the platform user ID.
+ */
+const getStudentCodeByPlatformUserId = async (platform_user_id) => {
+    // for debugging purpose
+    // console.log("getStudentCodeByPlatformUserId called");
+    // console.log("platform_user_id:", platform_user_id);
+    const result = await database.one(
+        `SELECT student_code 
+         FROM hasstudentprofilein 
+         WHERE platform_user_id = $1`,
+        [platform_user_id]
+    );
+    return result.student_code;
+}
 
 module.exports = {
     createHasStudentProfileIn,
+    getStudentCodeByPlatformUserId
 }
