c395eafed1f69fa80eb8ae1b3734603a0075dc51
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2025-06-11 16:22:58 +0100

    feat: add initial Jest configuration for testing

---
+++ backend/jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.js'],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  verbose: true,
+};
