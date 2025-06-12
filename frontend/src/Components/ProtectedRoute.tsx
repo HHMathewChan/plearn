e5286bb0ba5bd088020ce30c9d013897eaaa7188
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2025-06-12 18:15:39 +0100

    refactor: update ProtectedRoute to use AuthService for authentication check

--- frontend/src/Components/ProtectedRoute.tsx
+++ frontend/src/Components/ProtectedRoute.tsx
@@ -1,14 +1,22 @@
+/**
+ * This file defines a ProtectedRoute component that checks for user authentication.
+ * If the user is not authenticated (i.e., no token is found in sessionStorage),
+ * it redirects the user to the login page.
+ * If the user is authenticated, it renders the children components.
+ */
 import { Navigate } from "react-router-dom";
+import { AuthService } from "../Services/AuthService";
 
-const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
-    const token = sessionStorage.getItem('token');
-
-    // If token is not set, redirect to login page
-    if (!token) {
+type ProtectedRouteProps = {
+    children: JSX.Element;
+}
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+    // If isAuthenticated is false, redirect to login page
+    if (!AuthService.isAuthenticated()) {
         return <Navigate to="/login" replace={true} />;
     }
 
-    // If token is set, render the children components
+    // If isAuthenticated is true, render the children components
     return children;
 }
 
