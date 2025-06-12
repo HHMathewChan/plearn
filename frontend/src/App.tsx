fa56b3b581f429a83bb27c0d106dd39feb401065
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2025-06-12 18:15:12 +0100

    fix: update login route to use LoginFacade component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter,Routes, Route} from 'react-router-dom';
 import StudentHome from './Pages/StudentHome';
 import LandingPage from './Pages/LandingPage';
-import Login from './UseCases/Login';
+import { LoginFacade } from './UseCases/LoginFacade';
 import ProtectedRoute from './Components/ProtectedRoute';
 import RegisterFacade from './UseCases/RegisterFacade';
 
@@ -12,16 +12,17 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<LoginFacade />} />
           <Route path="/register" element={<RegisterFacade />} />
-          {/* Protected route for student home */}
+          // Protected route for student home 
           <Route 
             path="/student-home" 
             element={
               <ProtectedRoute>
                 <StudentHome />
               </ProtectedRoute>
-            } />
+            }
+          />
         </Routes>
       </BrowserRouter>
     </div>
