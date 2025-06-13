d83ec2c663bbbde256c9eac23676d1d714588bc4
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2025-06-13 15:43:42 +0100

    refactor: remove student registration and login routes from platformUserRoutes

--- backend/routes/platformUserRoutes.js
+++ backend/routes/platformUserRoutes.js
@@ -4,8 +4,6 @@ const studentController = require('../controllers/studentController');
 const platformUserController = require('../controllers/platformUserController');
 
 router.get('/students', studentController.getAllStudents);
-router.post('/students', studentController.registerStudent);
-router.post('/students/email', studentController.verifyStudentLogin);
 
 router.post('/platform-users/email', platformUserController.verifyPlatformUserLogin);
 router.post('/platform-users', platformUserController.registerPlatformUser);
