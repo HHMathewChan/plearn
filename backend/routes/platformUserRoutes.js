e2962da683c6b21c8a8d85af787d46086629f7b4
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2025-06-13 14:35:18 +0100

    fix: add platform user login route to platformUserRoutes

--- backend/routes/platformUserRoutes.js
+++ backend/routes/platformUserRoutes.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const studentController = require('../controllers/studentController');
+const platformUserController = require('../controllers/platformUserController');
 
 router.get('/students', studentController.getAllStudents);
 router.post('/students', studentController.registerStudent);
 router.post('/students/email', studentController.verifyStudentLogin);
+
+// Uncomment the following lines when user management is implemented
+// router.get('/users', userController.getAllUsers);
+// router.post('/users', userController.registerUser);
+router.post('/platform-users/email', platformUserController.verifyPlatformUserLogin);
 
 module.exports = router;
