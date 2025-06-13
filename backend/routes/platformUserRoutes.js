76c687cc42686bddf3c9bd95352053e6a432789b
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2025-06-13 15:40:40 +0100

    feat: refractor student register routers to platformUserRoutes

--- backend/routes/platformUserRoutes.js
+++ backend/routes/platformUserRoutes.js
@@ -7,9 +7,7 @@ router.get('/students', studentController.getAllStudents);
 router.post('/students', studentController.registerStudent);
 router.post('/students/email', studentController.verifyStudentLogin);
 
-// Uncomment the following lines when user management is implemented
-// router.get('/users', userController.getAllUsers);
-// router.post('/users', userController.registerUser);
 router.post('/platform-users/email', platformUserController.verifyPlatformUserLogin);
+router.post('/platform-users', platformUserController.registerPlatformUser);
 
 module.exports = router;
