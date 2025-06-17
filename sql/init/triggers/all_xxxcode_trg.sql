17e6949b17611c31aba9d024d67f1d161b5a89ee
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2025-06-17 17:47:30 +0100

    feat: add trigger for auto-incrementing enrollment code in Enrolment table

--- sql/init/triggers/all_xxxcode_trg.sql
+++ sql/init/triggers/all_xxxcode_trg.sql
@@ -5,3 +5,11 @@ CREATE TRIGGER trg_generate_student_code
 BEFORE INSERT ON Student
 FOR EACH ROW
 EXECUTE FUNCTION generate_student_code();
+
+/** aim: attach generate_enrollment_code() function to the enrollment code
+precondition 1: generate_enrollment_code() function is created
+postcondition 1: enrollment_code attribute can be auto incremented**/
+CREATE TRIGGER trg_generate_enrolment_code
+BEFORE INSERT ON Enrolment
+FOR EACH ROW
+EXECUTE FUNCTION generate_enrolment_code();
