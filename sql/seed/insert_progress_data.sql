95c0b692fd3c0eeac2d6d2990b6afa4f8e9d3f90
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2025-08-25 11:44:27 +0100

    fix: remove commented-out SQL insert statements and update final quiz log entry

--- sql/seed/insert_progress_data.sql
+++ sql/seed/insert_progress_data.sql
@@ -8,25 +8,4 @@ INSERT INTO logpastattemptforstudentat (quiz_attempt_id, student_code) VALUES
 
 -- Log past attempt for final quiz with STR20252
 INSERT INTO logpastattemptforfinalquizwith (final_quiz_id, quiz_attempt_id) VALUES
-('b3b1c7e2-ffff-4e1a-9c1a-ffffffffffff', 'b3b1c7e2-aaaa-4e1a-9c1a-aaaaaaaaaaaa');
-
--- The following is for testing
--- -- Student answer for STR20252
--- INSERT INTO studentanswer (id, selected_option_id, is_correct) VALUES
--- ('b3b1c7e2-bbbb-4e1a-9c1a-bbbbbbbbbbbb', 'b3b1c7e2-cccc-4e1a-9c1a-cccccccccccc', NULL);
-
--- -- Log active quiz attempt for STR20252
--- INSERT INTO logactiveattemptforstudentat (quiz_attempt_id, student_code) VALUES
--- ('b3b1c7e2-aaaa-4e1a-9c1a-aaaaaaaaaaaa', 'STR20252');
-
--- -- Log student answer for STR20252
--- INSERT INTO loganswerforattemptwith (quiz_attempt_id, student_answer_id) VALUES
--- ('b3b1c7e2-eeee-4e1a-9c1a-eeeeeeeeeeee', 'b3b1c7e2-bbbb-4e1a-9c1a-bbbbbbbbbbbb');
-
--- -- Log answer for final quiz with STR20252
--- INSERT INTO LogAnswerForOptionWith (question_option_id, student_answer_id) VALUES
--- ('b3b1c7e2-cccc-4e1a-9c1a-cccccccccccc', 'b3b1c7e2-bbbb-4e1a-9c1a-bbbbbbbbbbbb');
-
--- -- Log active attempt for final quiz with STR20252
--- INSERT INTO logactiveattemptforfinalquizwith (final_quiz_id, quiz_attempt_id) VALUES
--- ('b3b1c7e2-ffff-4e1a-9c1a-ffffffffffff', 'b3b1c7e2-aaaa-4e1a-9c1a-aaaaaaaaaaaa');
+('99999999-0009-4e1a-9c1a-333333333339', 'b3b1c7e2-aaaa-4e1a-9c1a-aaaaaaaaaaaa');
