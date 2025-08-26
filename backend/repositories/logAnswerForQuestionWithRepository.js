fb933c017409370f604631b1ef6011d3d523998d
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2025-08-26 18:05:03 +0100

    fix: update createRecord to use correct parameter name and SQL table, add getRecord function

--- backend/repositories/logAnswerForQuestionWithRepository.js
+++ backend/repositories/logAnswerForQuestionWithRepository.js
@@ -2,18 +2,38 @@ const database = require('../database');
 
 /**
  * Create a new log entry for answers associated with a quiz attempt.
- * @param {number} questionOptionId - The ID of the question option.
+ * @param {number} questionId - The ID of the question option.
  * @param {number} studentAnswerId - The ID of the student answer.
  * @returns {Promise<Object>} - The created log entry.
  */
-const createRecord = async (questionOptionId, studentAnswerId) => {
+const createRecord = async (questionId, studentAnswerId) => {
     const result = await database.query(
-        'INSERT INTO loganswerforoptionwith (question_option_id, student_answer_id) VALUES ($1, $2) RETURNING *',
-        [questionOptionId, studentAnswerId]
+        'INSERT INTO loganswerforquestionwith (question_id, student_answer_id) VALUES ($1, $2) RETURNING *',
+        [questionId, studentAnswerId]
     );
     return result[0];
 }
 
+/**
+ * Get the record using the student answer ID.
+ * @param {number} studentAnswerId - The ID of the student answer.
+ * @returns {Promise<Object>} - The log entry record.
+ */
+const getRecord= async (studentAnswerId) => {
+    console.log('At logAnswerForQuestionWithRepository, getRecord is called for:', { studentAnswerId });
+    try {
+        const result = await database.oneOrNone(
+            'SELECT * FROM loganswerforquestionwith WHERE student_answer_id = $1',
+            [studentAnswerId]
+        );
+        return result;
+    } catch (error) {
+        console.error('Error getting record from loganswerforquestionwith:', error);
+        throw error;
+    }
+}
+
 module.exports = {
-    createRecord
+    createRecord,
+    getRecord
 };
