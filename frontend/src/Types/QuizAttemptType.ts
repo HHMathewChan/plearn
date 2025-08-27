2d896886983a80fd56ac8fd6d1874c4355435db6
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2025-08-27 11:08:47 +0100

    feat: add quizAttempt type definition for quiz attempt data structure

---
+++ frontend/src/Types/QuizAttemptType.ts
@@ -0,0 +1,11 @@
+type quizAttempt = {
+id: string;
+score: number;
+started_at: string;
+completed_at: string;
+attempt_count: number;
+attempt_status: string;
+updated_at: string;
+}
+
+export type { quizAttempt };
