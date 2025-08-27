b1e2ccb4b375b0a819580ba713f99869bac69904
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2025-08-27 17:00:10 +0100

    feat: add course progress routes to server configuration

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ const courseContentRoutes = require('./routes/courseContentRoutes');
 const contentProgressRoutes = require('./routes/contentProgressRoutes');
 const finalQuizRoutes = require('./routes/finalQuizRoutes');
 const quizAttemptRoutes = require('./routes/quizAttemptRoutes');
+const courseProgressRoutes = require('./routes/courseProgressRoutes.js');
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -27,6 +28,7 @@ app.use('/api/course-content-routes', courseContentRoutes);
 app.use('/api/content-progress-routes', contentProgressRoutes);
 app.use('/api/final-quiz-routes', finalQuizRoutes);
 app.use('/api/quiz-attempt-routes', quizAttemptRoutes);
+app.use('/api/course-progress-routes', courseProgressRoutes);
 
 // Serve course resources statically
 app.use('/course-resources', express.static(path.join(__dirname, 'courseResources')));
