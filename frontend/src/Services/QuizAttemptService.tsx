231fc36681b60806df95a46901d2ff302ada0a0a
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2025-08-27 10:29:17 +0100

    feat: add submitQuizAttempt function for final quiz submission

--- frontend/src/Services/QuizAttemptService.tsx
+++ frontend/src/Services/QuizAttemptService.tsx
@@ -2,6 +2,8 @@ import {AuthService} from './AuthService';
 
 const API_BASE_URL = 'http://localhost:3001/api/quiz-attempt-routes';
 
+type optionId = string;
+
 export const createQuizAttempt = async (studentCode: string, courseId: string) => {
     const isAuth = AuthService.isAuthenticated();
     if (!isAuth) {
@@ -28,6 +30,36 @@ export const createQuizAttempt = async (studentCode: string, courseId: string) =
         return data;
     } catch (error) {
         console.error('At QuizAttemptService.tsx: Error fetching final quiz:', error);
+        throw error;
+    }
+}
+
+export const submitQuizAttempt = async (courseId: string, studentCode: string, answers: { [questionId: string]: optionId }) => {
+    const isAuth = AuthService.isAuthenticated();
+    if (!isAuth) {
+        throw new Error('User is not authenticated. Please log in again.');
+    }
+    try {
+        const response = await fetch(`${API_BASE_URL}/quiz-attempt`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ 
+                action: 'complete',
+                course_id: courseId, 
+                student_code: studentCode,
+                student_answers: answers
+            })
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log('At QuizAttemptService.tsx: Final quiz submitted:', data);
+        return data;
+    } catch (error) {
+        console.error('At QuizAttemptService.tsx: Error submitting final quiz:', error);
         throw error;
     }
 }
