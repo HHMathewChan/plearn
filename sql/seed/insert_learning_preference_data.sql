6d85d3285926a85ccd06d4053cf76fd5b5a31955
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2025-09-02 17:37:50 +0100

    feat: add SQL script to insert learning modes into the database

---
+++ sql/seed/insert_learning_preference_data.sql
@@ -0,0 +1,5 @@
+-- Learning Modes
+INSERT INTO learningmode (id, mode_name) VALUES
+('7543e822-b3df-4714-8913-07ddfc02cbbd', 'beginner'),
+('55523710-7c5c-4940-897b-d892ae82600f', 'intermediate'),
+('12ce5c1b-2b2e-4ad1-bbd7-a5a2a01151b6', 'challenging');
