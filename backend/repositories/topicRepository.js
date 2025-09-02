0cfd0eef8957b006b1fe9cae1a3b844d4c313cbf
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2025-09-02 17:37:28 +0100

    feat: add repository for retrieving all topics from the database

---
+++ backend/repositories/topicRepository.js
@@ -0,0 +1,18 @@
+const database = require('../database');
+
+/**
+ * Get all the topics.
+ * @returns {Promise} - A promise that resolves to an array of topics.
+ */
+const getAllTopics = async () => {
+  // for debugging
+  console.log('At topicRepository, Retrieving all topics from database...');
+  const result = await database.any('SELECT * FROM topics');
+  // for debugging
+  console.log('Retrieved all topics from database:', result);
+  return result;
+};
+
+module.exports = {
+  getAllTopics,
+};
