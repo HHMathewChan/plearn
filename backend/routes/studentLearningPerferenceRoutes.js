f4beab158decbf61ee4604736a02ce7541ab2ea6
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2025-09-05 18:07:15 +0100

    fix: update route to use correct student learning preference controller

--- backend/routes/studentLearningPerferenceRoutes.js
+++ backend/routes/studentLearningPerferenceRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const studentLearningPreferenceController = require('../controllers/studentLearningPerferenceController');
 
-router.post('/student-learning-preference', studentLearningPreferenceController.createStudentLearningPreferences);
+router.post('/student-learning-preference', studentLearningPreferenceController.studentLearningPerferenceController);
 
 module.exports = router;
