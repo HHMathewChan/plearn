d109d9f4e4ad52e49149835f2593923360d84036
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2025-09-07 17:50:48 +0100

    feat: add getModeNameById function to retrieve learning mode name by ID

--- backend/repositories/learningModeRepository.js
+++ backend/repositories/learningModeRepository.js
@@ -13,6 +13,21 @@ const getAllLearningModes = async () => {
   return result;
 };
 
+/**
+ * Get the learning mode name by its ID.
+ * @param {number} Id - The ID of the learning mode.
+ * @returns {Promise<string|null>} - A promise that resolves to the mode name or null if not found.
+ */
+const getModeNameById = async (Id) => {
+  // for debugging
+  console.log('At learningModeRepository, Retrieving mode name by ID from database...');
+  const result = await database.oneOrNone('SELECT mode_name FROM learningmode WHERE id = $1', [Id]);
+  // for debugging
+  console.log('Retrieved mode name from database:', result);
+  return result ? result.mode_name : null;
+};
+
 module.exports = {
   getAllLearningModes,
+  getModeNameById
 };
