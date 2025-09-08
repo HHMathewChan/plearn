39eacc0bc25975251565a3d373a0682734cb86a6
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2025-09-08 22:28:35 +0100

    feat: extend CourseType with CourseWithRecommendation and RecommendedCourses types

--- frontend/src/Types/CourseType.ts
+++ frontend/src/Types/CourseType.ts
@@ -17,5 +17,21 @@ export type CourseWithMetadata = {
   topic: Topic;
 };
 
+export type CourseWithMetadata2 = {
+  id: string;
+  course_code: string;
+  title: string;
+  difficulty: string;
+  description: string;
+  copyrightOwner: CopyrightOwner;
+  topic: Topic;
+};
+
+export type CourseWithRecommendation = {
+  course: CourseWithMetadata2;
+  score: number;
+}
+
 // The type representing an array of courses with their owners
 export type Courses = CourseWithMetadata[];
+export type RecommendedCourses = CourseWithRecommendation[];
