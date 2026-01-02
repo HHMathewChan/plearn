ec8b3e9a914e402d5c879c39af2a4eae6ba8ef71
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2026-01-02 16:26:17 +0000

    refactor: remove unused route for fetching course materials

--- backend/routes/courseContentRoutes.js
+++ backend/routes/courseContentRoutes.js
@@ -6,6 +6,5 @@ const courseContentController = require('../controllers/courseContentController'
 router.get('/:courseId/course-contents', courseContentController.getCourseContentByCourseId);
 router.get('/course-contents/:contentId', courseContentController.getCourseContentById);
 router.get('/course-contents/:contentId/signed-url', courseContentController.getSignedContentUrl);
-router.get('/course-materials/*contentUrl', courseContentController.getCourseMaterialUrl);
 
 module.exports = router;
