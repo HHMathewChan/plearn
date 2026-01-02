d9f7e0f83b39558bf62182f0a8eea12f9b2c7cec
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2026-01-02 15:11:05 +0000

    refactor: update documentation for generateSignedCourseUrl function

--- backend/infrastructure/storage/r2Service.js
+++ backend/infrastructure/storage/r2Service.js
@@ -2,6 +2,13 @@ const { GetObjectCommand } = require('@aws-sdk/client-s3');
 const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
 const r2Client = require('./r2Client');
 
+/**
+ * Generate a signed URL for accessing a course material file stored in R2.
+ * @async
+ * @function generateSignedCourseUrl
+ * @param {string} filename - The name of the file to generate a signed URL for
+ * @returns {Promise<string>} - A promise that resolves to the signed URL
+ */
 async function generateSignedCourseUrl(filename) {
     //for debugging purpose
     console.log("generateSignedCourseUrl called with filename:", filename);
