1b9cf795a2bd71bf8232e27c736997b33ab9fbd7
Ho Hin Mathew Chan <105818584+HHMathewChan@users.noreply.github.com>
2026-01-02 16:24:08 +0000

    feat: add route for generating signed URLs for course contents

--- backend/routes/courseContentRoutes.js
+++ backend/routes/courseContentRoutes.js
@@ -5,6 +5,7 @@ const courseContentController = require('../controllers/courseContentController'
 // Course content routes
 router.get('/:courseId/course-contents', courseContentController.getCourseContentByCourseId);
 router.get('/course-contents/:contentId', courseContentController.getCourseContentById);
+router.get('/course-contents/:contentId/signed-url', courseContentController.getSignedContentUrl);
 router.get('/course-materials/*contentUrl', courseContentController.getCourseMaterialUrl);
 
 module.exports = router;
