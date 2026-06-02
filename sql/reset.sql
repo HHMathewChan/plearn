b6d2739fb6e44d8aaa10b42d8fd470a51b08a647
Ho Hin Mathew Chan <todayisniceandgood@gmail.com>
2026-06-02 17:50:05 +0100

    fix: update file paths in reset.sql for consistency with project structure

--- sql/reset.sql
+++ sql/reset.sql
@@ -1,23 +1,23 @@
 -- Step one: drop first
-\i /Users/mathewchan/plearn/sql/drop/drop_triggers.sql
-\i /Users/mathewchan/plearn/sql/drop/drop_functions.sql
-\i /Users/mathewchan/plearn/sql/drop/drop_sequences.sql
-\i /Users/mathewchan/plearn/sql/drop/drop_tables.sql
-\i /Users/mathewchan/plearn/sql/drop/drop_domains.sql
+\i /Users/mathewchan/Projects/plearn/sql/drop/drop_triggers.sql
+\i /Users/mathewchan/Projects/plearn/sql/drop/drop_functions.sql
+\i /Users/mathewchan/Projects/plearn/sql/drop/drop_sequences.sql
+\i /Users/mathewchan/Projects/plearn/sql/drop/drop_tables.sql
+\i /Users/mathewchan/Projects/plearn/sql/drop/drop_domains.sql
 
 -- Step two: recreate
-\i /Users/mathewchan/plearn/sql/init/create_domains.sql
-\i /Users/mathewchan/plearn/sql/init/create_tables_1.sql
-\i /Users/mathewchan/plearn/sql/init/create_tables_2.sql
-\i /Users/mathewchan/plearn/sql/init/create_sequences.sql
-\i /Users/mathewchan/plearn/sql/init/create_functions.sql
-\i /Users/mathewchan/plearn/sql/init/triggers/all_xxxcode_trg.sql
+\i /Users/mathewchan/Projects/plearn/sql/init/create_domains.sql
+\i /Users/mathewchan/Projects/plearn/sql/init/create_tables_1.sql
+\i /Users/mathewchan/Projects/plearn/sql/init/create_tables_2.sql
+\i /Users/mathewchan/Projects/plearn/sql/init/create_sequences.sql
+\i /Users/mathewchan/Projects/plearn/sql/init/create_functions.sql
+\i /Users/mathewchan/Projects/plearn/sql/init/triggers/all_xxxcode_trg.sql
 
 -- Step three: insert initial data
-\i /Users/mathewchan/plearn/sql/seed/insert_learning_preference_data.sql
-\i /Users/mathewchan/plearn/sql/seed/insert_course_data.sql
-\i /Users/mathewchan/plearn/sql/seed/insert_platformuser_data.sql
-\i /Users/mathewchan/plearn/sql/seed/insert_final_quiz_related.sql
-\i /Users/mathewchan/plearn/sql/seed/insert_progress_data.sql
--- \i /Users/mathewchan/plearn/sql/seed/insert_student_profolio_test_data.sql
-\i /Users/mathewchan/plearn/sql/seed/insert_student_profolio_test_data_2.sql
+\i /Users/mathewchan/Projects/plearn/sql/seed/insert_learning_preference_data.sql
+\i /Users/mathewchan/Projects/plearn/sql/seed/insert_course_data.sql
+\i /Users/mathewchan/Projects/plearn/sql/seed/insert_platformuser_data.sql
+\i /Users/mathewchan/Projects/plearn/sql/seed/insert_final_quiz_related.sql
+\i /Users/mathewchan/Projects/plearn/sql/seed/insert_progress_data.sql
+-- \i /Users/mathewchan/Projects/plearn/sql/seed/insert_student_profolio_test_data.sql
+\i /Users/mathewchan/Projects/plearn/sql/seed/insert_student_profolio_test_data_2.sql
